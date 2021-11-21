4941f7ecfb88074bdd4cf8d291f798d908a8bbae
gadjetron <gi87365@yandex.ru>
2021-11-21 18:16:47 +0200

    button example

---
+++ button.py
@@ -0,0 +1,27 @@
+from kivy.uix.screenmanager import Screen
+
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRectangleFlatButton
+
+
+class MainApp(MDApp):
+    def build(self):
+        screen = Screen()
+        
+        screen.add_widget(
+            MDRectangleFlatButton(
+                text="Hello, World from button 1",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            )
+        )
+        
+        screen.add_widget(
+            MDRectangleFlatButton(
+                text="Hello, World from button 2",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            )
+        )
+        return screen
+
+
+MainApp().run()
